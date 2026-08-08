23b3956bb46790852df9fe6a2072aa61d4f63fd6
Nikhil <nikhilbankar3839@gmail.com>
2026-08-08 23:14:24 +0530

    Added Playwright automation framework

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,16 @@
+import {test, expect} from "@playwright/test";
+
+//fixture- global variable : page, browser 
+
+test("verify page title", async ({page})=>{
+
+    await page.goto("https://www.amazon.in/");
+    let title:String=  await page.title();
+     console.log("title", title);
+
+    await expect(page).toHaveTitle("Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
+
+
+}
+
+)
---
+++ tests/mytest1.spec.ts
@@ -0,0 +1,12 @@
+import {test,expect} from "@playwright/test";
+
+test("verify page url", async({page})=>{
+
+await page.goto("https://www.amazon.in/");
+
+let url:String =  page.url();
+console.log("url->",url);
+
+await  expect(page).toHaveURL("https://www.amazon.in/");
+
+})
