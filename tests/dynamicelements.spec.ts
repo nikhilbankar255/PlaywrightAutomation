e59d8913fa146c11a2332f6b0d053609507b099e
Nikhil <nikhilbankar3839@gmail.com>
2026-08-09 23:41:01 +0530

    File dynamicelements using css and playwright locator

--- tests/dynamicelements.spec.ts
+++ tests/dynamicelements.spec.ts
@@ -1,6 +1,6 @@
 import {test,expect,Locator} from "@playwright/test"
 
-
+//using xpath
 test("Handle dynamic elements using xpath", async({page})=>{
 
     await page.goto("https://testautomationpractice.blogspot.com/");
@@ -9,6 +9,42 @@ test("Handle dynamic elements using xpath", async({page})=>{
     {
 
             let button:Locator=page.locator("//button[text()='START' or text()='STOP']");
+
+            await button.click();
+
+            await page.waitForTimeout(2000);
+    }
+
+})
+
+
+//using css
+test("Handle dynamic elements using css", async({page})=>{
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    for(let i=1; i<=5; i++)
+    {
+
+            let button:Locator=page.locator("button[name='start'],button[name=stop]");
+
+            await button.click();
+
+            await page.waitForTimeout(2000);
+    }
+
+})
+
+
+//using playwright locator getByRole
+test("Handle dynamic elements using Palywright locator", async({page})=>{
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    for(let i=1; i<=5; i++)
+    {
+
+            let button:Locator=page.getByRole('button', {name: /START|STOP/});
 
             await button.click();
 
