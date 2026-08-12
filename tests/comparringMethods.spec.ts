b2065086be39407c58883aa682331b08e6f9db78
Nikhil <nikhilbankar3839@gmail.com>
2026-08-12 14:13:22 +0530

    spaces removed

--- tests/comparringMethods.spec.ts
+++ tests/comparringMethods.spec.ts
@@ -7,7 +7,7 @@ test("Comparring Methods",async({page})=>{
     
     //1. innerText() vs textContent()
     console.log(await products.nth(1).innerText());
-        console.log(await products.nth(1).textContent());
+    console.log(await products.nth(1).textContent());
 
     const count=await products.count();
         for(let i=0;i<count;i++)
@@ -17,23 +17,23 @@ test("Comparring Methods",async({page})=>{
           console.log(productText?.trim());  //spaces and lines. to use the text we have to use trim() using map.
         }
 
-     //2. allinnerText() vs allTextContent()
-     const productallinnerTexts:string[]=await products.allInnerTexts();
-     console.log("innerText()->",productallinnerTexts);
+    //2. allinnerText() vs allTextContent()
+    const productallinnerTexts:string[]=await products.allInnerTexts();
+    console.log("innerText()->",productallinnerTexts);
 
-      const productallTextcontents:string[]=(await products.allTextContents()).map((text=>text.trim()));
-     console.log("textContents()->",productallTextcontents);
+    const productallTextcontents:string[]=(await products.allTextContents()).map((text=>text.trim()));
+    console.log("textContents()->",productallTextcontents);
 
      //3. all() ---> array of locators[]
-     const allProductslocators:Locator[]=await products.all();
+    const allProductslocators:Locator[]=await products.all();
     
-     console.log("printing in all() loop of=>");
-     for(let product of allProductslocators)
-     {
+    console.log("printing in all() loop of=>");
+    for(let product of allProductslocators)
+    {
         console.log(await product.innerText());
-     }
+    }
 
-          console.log("printing in all() loop in=>");
+    console.log("printing in all() loop in=>");
 
      for(let i in allProductslocators)
      {
