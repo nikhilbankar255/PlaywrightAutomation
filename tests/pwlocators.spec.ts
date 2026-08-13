1ab972fb70c969eac70beae470c4132f6a06ac26
Nikhil <nikhilbankar3839@gmail.com>
2026-08-13 18:06:51 +0530

    added wait and removed expired element

--- tests/pwlocators.spec.ts
+++ tests/pwlocators.spec.ts
@@ -1,36 +1,34 @@
-import {test,expect,Locator} from "@playwright/test";
+import { test, expect, Locator } from "@playwright/test";
 
 
 //1. getByAltText()
-test("Verify Playwright Locators", async({page})=>{
+test("Verify Playwright Locators", async ({ page }) => {
 
 
-   await page.goto("https://www.amazon.in/");
+    await page.goto("https://www.amazon.in/");
 
-    const logo:Locator= page.getByAltText("AugART26_Event")
+    await page.waitForTimeout(2000);
+    const logo: Locator = page.getByAltText("ACs")
     await expect(logo).toBeVisible();
-    
-//2.getByVisibleText()
-         // const text: Locator= page.getByText("Welcome to our store");
-         // await expect(text).toBeVisible();
-    await expect(page.getByText("Blockbuster Deals are live | Shop now")).toBeVisible();
 
-//3. getByRole() here it's not working so used css. can try with other website
-    const role:Locator=page.locator('#nav-link-accountList-nav-line-1');
+    //2.getByVisibleText()
+    // const text: Locator= page.getByText("Welcome to our store");
+    // await expect(text).toBeVisible();
+    await expect(page.getByText("Starting ₹199 | Amazon Brands & more")).toBeVisible();
+
+    //3. getByRole() here it's not working so used css. can try with other website
+    const role: Locator = page.locator('#nav-link-accountList-nav-line-1');
     await role.click();
-    await expect(page.getByRole("heading", {name: 'Sign in or create account' })).toBeVisible();
-
+    await expect(page.getByRole("heading", { name: 'Sign in or create account' })).toBeVisible();
 
     //4.getByLabel()
-
     await page.getByLabel("Enter mobile number or email").fill("test@test.com");
-
 
     //5. getByPlaceholder()
     await page.getByLabel("Amazon").click();
     await page.getByPlaceholder("Search Amazon.in").fill("mobile");
 
     //6. getByTitle()
-    const searchBox:Locator=page.getByTitle("Search in");
+    const searchBox: Locator = page.getByTitle("Search in");
     await expect(searchBox).toBeVisible();
 })
