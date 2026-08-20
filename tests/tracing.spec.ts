5b1cb2b29d7282955464f8afff2849c70d4ad95b
Nikhil <nikhilbankar3839@gmail.com>
2026-08-20 13:43:37 +0530

    File trace viewer added

--- tests/tracing.spec.ts
+++ tests/tracing.spec.ts
@@ -1,5 +1,19 @@
 import{test, expect} from "@playwright/test"
 
+/*
+1. Using playwright.config file
+2. using command line --trace=on
+3. code(programatically) -(no html report) 
+     context.tracing.start({screenshots:true,snapshots:true});
+    //statements
+     context.tracing.stop({path:'trace.zip'});
+
+     To view trace file--->
+     1) from html file-> click on trace.zip
+     2) through command -> npx playwright show-trace trace.zip
+     3) using utility-> https://trace.playwright.dev/
+*/
+
 test("tracing test", async({page})=>{
 
     await page.goto("https://www.demoblaze.com/");
@@ -11,4 +25,19 @@ test("tracing test", async({page})=>{
     await page.getByRole('button', { name: 'Log in' }).click();
     await expect(page.getByRole('link', { name: 'Welcome pavano1' })).toBeVisible();
     await page.getByRole('link', { name: 'Log out' }).click();
-})
+});
+
+test("context tracing test", async({page,context})=>{
+
+    context.tracing.start({screenshots:true,snapshots:true});
+    await page.goto("https://www.demoblaze.com/");
+    await page.getByRole('link', { name: 'Log in' }).click();
+    await page.locator('#loginusername').click();
+    await page.locator('#loginusername').fill('pavano1');
+    await page.locator('#loginusername').press('Tab');
+    await page.locator('#loginpassword').fill('test@123');
+    await page.getByRole('button', { name: 'Log in' }).click();
+    await expect(page.getByRole('link', { name: 'Welcome pavano1' })).toBeVisible();
+    await page.getByRole('link', { name: 'Log out' }).click();
+    context.tracing.stop({path:'trace.zip'});
+});
